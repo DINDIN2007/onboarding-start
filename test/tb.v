1ec8bf7243044f0ebccb79fe75fae9159df77ad7
DINDIN2007 <dinhvietluong99@gmail.com>
2025-09-02 21:42:06 -0400

    Initial Commit

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_uwasic_onboarding_dinh_viet_luong user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
