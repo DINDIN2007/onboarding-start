2356cc9ab16b31e24edc60ce2b31340d3cfa82b7
DINDIN2007 <dinhvietluong99@gmail.com>
2025-09-22 23:57:40 -0400

    Solved conflicting drivers on the same signal

--- src/project.v
+++ src/project.v
@@ -18,9 +18,6 @@ module tt_um_uwasic_onboarding_dinh_viet_luong (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_oe = 8'hFF;
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
   // Create wires to refer to the values of the registers
   wire [7:0] en_reg_out_7_0;
